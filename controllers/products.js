c4ab2b5c8c74fbd9fd11031aad3fba5ca9a66a9e
hafizip00 <hafizip00@gmail.com>
2022-07-05 19:21:45 +0500

    Filtering where you can search by name and sort by name navigate by paage number limit products and search selected fields

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 const getAllptoducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -12,14 +12,30 @@ const getAllptoducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+  //Sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    result: result.select(fieldList);
+  }
+  const limit = Number(req.query.limit) || 10;
+  const page = Number(req.query.page) || 1;
+  const skip = (page - 1) * limit;
+  result: result.skip(skip).limit(limit);
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllptoductsStatic = async (req, res) => {
   const search = "aa";
-  const products = await Product.find({});
-  res.status(200).json({ products });
+  const products = await Product.find({}).select("name price").limit(4);
+  res.status(200).json({ products, q: products.length });
 };
 
 module.exports = {
